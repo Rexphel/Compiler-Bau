ac8bb87715f0ffec818f96b3d910beb8349ceb62
Rexphel <philiprexroth@gmail.com>
2022-12-13 16:28:57 +0100

    inital maven setup

---
+++ Compliler-Test/src/main/java/org/Junit/ABC.java
@@ -0,0 +1,9 @@
+package org.Junit;
+
+public class ABC {
+    public ABC(){}
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ Compliler-Test/src/main/java/org/Junit/CompilerTest.java
@@ -0,0 +1,12 @@
+package org.Junit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CompilerTest {
+    private final ABC abc = new ABC();
+    @Test
+    public void abcd(){
+        assertEquals(2, abc.add(1,1));
+    }
+}
